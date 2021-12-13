77fef186f31216131fe85ba18442ba28a71825c9
thebosph <haciibrahimsimsek@ogr.iu.edu.tr>
2021-12-13 23:49:34 +0300

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="w-full h-screen">
+      <Head>
+        <title>Bi'Gittim</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
